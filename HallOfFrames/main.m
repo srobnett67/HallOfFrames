87eb8949e0e2989d50e1ae721755cf6f9e7de245
Scot Robnett <okgetserious@gmail.com>
2016-03-23 14:10:21 -0500

    Initial Commit

---
+++ HallOfFrames/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HallOfFrames
+//
+//  Created by Scot Robnett on 3/23/16.
+//  Copyright © 2016 Scot Robnett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
